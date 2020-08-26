0b12f2a4e4fb581810f63524c26c1eb72b703b75
surface@pro.3 <348699103@qq.com>
2020-08-26 21:31:43 +0800

    完成了24点

--- 凑成24点.py
+++ 凑成24点.py
@@ -17,10 +17,20 @@ def divide(a, b):
     return a / b
 
 
-def list_combination_with_left(in_list, r):
+operations = [add, subtract, multiply, divide]
+
+
+def equal(a, b):
+    if abs(a-b) < 10**-6:
+        return True
+    else:
+        return False
+
+
+def list_combination_with_left(in_list, r=2):
     length = len(in_list)
     result = []
-    for i in range(length-r):
+    for i in range(length-r+1):
         for j in range(i+1, length):
             list_ = copy(in_list)
             list_.pop(j)
@@ -28,4 +38,22 @@ def list_combination_with_left(in_list, r):
             result.append([(in_list[i], in_list[j]), list_])
     return result
 
-list_combination_with_left([1,2,3,4],2)
+
+def operate(a, b, func):
+    return func(a, b)
+
+
+def recursion_solve(question_list):
+    if len(question_list) == 1:
+        if equal(question_list[0], 24):
+            print(True)
+    for resolution in list_combination_with_left(question_list):
+        for operation in operations:
+            try:
+                recursion_solve(resolution[1] + [operate(*resolution[0], operation)])
+            except:
+                continue
+
+
+if __name__ == "__main__":
+    recursion_solve([1, 2, 1, 2])
